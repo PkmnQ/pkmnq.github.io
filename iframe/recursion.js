1daf3082dae4e51ef095542c1a8cd335a2885f54
PkmnQ <quantumivy737@gmail.com>
2022-01-06 21:13:01 +0800

    make the iframe look better

--- iframe/recursion.js
+++ iframe/recursion.js
@@ -1,8 +1,7 @@
 function addFrame() {
-	const recursiveFrame = document.createElement("iframe")
-	recursiveFrame.src = `iframe?r=${btoa(Math.random())}`
+	const recursiveFrame = document.createElement("iframe");
+	recursiveFrame.src = `iframe?r=${btoa(Math.random())}`;
 	// i only used btoa to make it look professional
 	// just using Math.random() probably works fine
-
-	document.body.appendChild(recursiveFrame)
+	document.body.appendChild(recursiveFrame);
 }
